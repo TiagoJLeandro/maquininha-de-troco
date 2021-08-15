494432b70cf622a6337ea8a4ddbec2b5156d1ce9
tiagojleandro <leandrotiago223@gmail.com>
2021-08-15 16:12:33 -0300

    refatorando a funcao que formata as entradas.

--- string_as_a_float.py
+++ string_as_a_float.py
@@ -2,14 +2,14 @@ from re import sub
 from re import compile as re_compile
 
 
-def string_to_float(value: str) -> str:
+def string_as_a_float(value: str) -> str:
     pattern = re_compile(
         "([0-9]{1,4})([,.])?([0-9]{1,2})?(.*)"
     )
     no_alpha_with_just_dot = sub(
         "^([^0-9]*)|([^0-9,.]*)|([^0-9,.]*)$", "", value
     )
-    just_one_dot = sub("[,.]{2,}", ".", no_alpha_with_just_dot)
+    just_one_dot = sub("[,.]{1,}", ".", no_alpha_with_just_dot)
     match = pattern.match(just_one_dot)
     result = ""
     if match:
