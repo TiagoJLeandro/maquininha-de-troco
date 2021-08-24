1a9383d6705dbe0509f967c9385672b4d878caa7
TiagoJLeandro <leandrotiago223@gmail.com>
2021-08-24 08:44:44 -0300

    arredondando o troco antes de decompor

--- calculate_change.py
+++ calculate_change.py
@@ -4,7 +4,7 @@ def calculate_change(list_coins, prod_value: float, received: float) -> list:
     if prod_value > received:
         return []
     result = []
-    change = received - prod_value
+    change = round(received - prod_value, 2)
     for coins in list_coins:
         change = round(change, 2) if change < 1 else change
         calc = int(Decimal(f'{change}') // Decimal(f'{coins}'))
